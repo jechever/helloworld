04df8dfc357bce6d09a0f88f64669cb5a37edec8
Jonathan Echevers <echevers@login4.colonialone.gwu.edu>
2014-12-16 15:54:25 -0500

    homework submission

--- bessel.c
+++ bessel.c
@@ -1,24 +1,23 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
 
 double x = 5.; 
 int n;
-//#define x 5.345
-
-double bessel(double x, int n); 
+extern double bessel(double x, int n); 
 
 int main(){
-  //  int n = 0;
-  //  printf("%lf\t%d\n",x,n);
   for (n = 0; n<10; n++){
-    printf("%lf %lf", x, bessel(x,n));
+    printf("%lf %lf\n", x, bessel(x,n));
   }
   return 0;
 } 
 
 double bessel(double x, int n){
   //nth term of a bessel func sequence
-  //if (0==n) return 0;
-  //if (1==n) return 1;
-  return ((2*(n-1)+1)*bessel(n-1) - x*bessel(n-2))/x;
+  if (n == 0 & x == 0) return 1;
+  if (n != 0 & x == 0) return 0;
+  if (n == 0) return sin(x)/x;
+  if (n == 1) return sin(x)/(x*x) - cos(x)/x;
+  return ((2*(n-1)+1)*bessel(n-1,x) - x*bessel(n-2,x))/x;
 }
--- precession_dummy.c
+++ precession_dummy.c
@@ -2,11 +2,10 @@
 #include <stdlib.h>
 #include <math.h>
 #define m_e 5.9736*pow(10,24)      //mass of the Earth
-#define m_s  1.9891*pow(10,30)     //mass of the Sun
-#define a  149598261.0             //Semi-major axis
+#define m_s  1.9891*pow(10,30)    //mass of the Sun
+#define a  149598261               //Semi-major axis
 #define e  0.0167112303531389      //Eccentricity 
 #define G  6.67428*pow(10,-11)     //Newton's constant
-#define KE ((G*m_e*m_s)/a)*((1/(1+e))-0.5)
 
 double function(double x, double v, double t, double d);
 double (*func)(double x, double v, double t, double d);
@@ -16,41 +15,38 @@ double function(double x, double v, double t, double d){
 }
 
 double functiony(double y, double vy, double t, double d);
-double (*funcy)(double y, double vy, double t, double d);
+double (*func)(double y, double vy, double t, double d);
 
 double functiony(double y, double vy, double t, double d){
   return (G*m_s/pow(d,3))*y; // integrand.
 }
 
 double rkn4(double (*func)(double x, double v, double t),
-	    double x[], double x0, double v[], double v0, double (*funcy)(double y, double vy, double t), double y[], double y0, double vy[], double vy0, double t, double h, double d, long steps);
+	double x[], double x0, double v[], double v0, double (*funcy)(double y, double vy, double t), double y[], double y0, double vy[], double vy0, double t, double h, long steps);
 
 
 
 int main (){
-	long steps = 10000000; 	       	//Number of steps.
-	double x0=a,v0=0.,t,h=0.000001; //Initial position, initial velocity in x, time, step size.
-	double y0=0.,vy0=sqrt((2*KE)/(m_e*(1+m_e/m_s)));//Initial position and  velocity in y-axis
-
+	long steps = 10000000; 				//Number of steps.
+	double x0=0.,v0=1.,t,h=0.000001; 			//Initial position, initial velocity, time, step size.
 	double *x = malloc(steps * sizeof(double)); 	//Position array
         double *v = malloc(steps * sizeof(double)); 	//Velocity array
 
+	double y0=1.,vy0=0.,; 			//Initial position, initial velocity, time, step size.
 	double *y = malloc(steps * sizeof(double)); 	//Position array
         double *vy = malloc(steps * sizeof(double)); 	//Velocity array
 /* Set pointer to point at the function to integrate. */
-
-	double d;
 
 	func = function;
 	funcy = functiony;
 
 /* Do integration. */
-	rkn4(func,x,x0,v,v0,funcy,y,y0,vy,vy0,t,h,d,steps);
+	rkn4(func,x,x0,v,v0,funcy,y,y0,vy,vy0,t,h,steps);
 	
 /* Print results to STDOUT */
 	long int i;
         for ( i=0; i<steps; ++i){
-	  printf(" %d %lf %lf %lf %lf \n",i,x[i],v[i],y[i],vy[i]);
+		printf(" %ld %e %e\n",i,x[i],v[i]);
         }
 
 
@@ -58,7 +54,7 @@ int main (){
 }
 
 double rkn4(double (*func)(double x, double v, double t), 
-	    double x[], double x0, double v[],double v0, double (*funcy)(double y, double vy, double t), double y[], double y0, double vy[],double vy0, double t, double h, double d, long steps)
+	double x[], double x0, double v[],double v0, double (*funcy)(double y, double vy, double t), double y[], double y0, double vy[],double vy0, double t, double h, long steps)
 {
 	double k1,k2,k3,k4;
 	double ky1,ky2,ky3,ky4;
